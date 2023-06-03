0df19b003f027ae9d33deccf79d4b7327b2e2e6d
navid.rashik <navid.rashik@gmail.com>
2023-06-03 19:46:59 +0600

    feat: customRenderer class added

--- utils/response_wrapper.py
+++ utils/response_wrapper.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-
+from rest_framework.renderers import JSONRenderer
 
 class ResponseWrapper(Response):
 
@@ -36,3 +36,43 @@ class ResponseWrapper(Response):
         super().__init__(data=output_data, status=status_by_default_for_gz,
                          template_name=template_name, headers=headers,
                          exception=exception, content_type=content_type)
+
+
+
+class CustomRenderer(JSONRenderer):
+    '''
+    to use it update settings file like this : 
+    REST_FRAMEWORK = {
+        "DEFAULT_RENDERER_CLASSES": ("utils.response_wrapper.CustomRenderer",),
+    }
+    '''
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        response = renderer_context["response"]
+
+        # if response.status_code == 401 or response.status_code == 405 or response.status_code == 403:
+        if "error" not in data and response.status_code >= 400 and response.status_code < 500:
+            # error
+            error_code = response.status_code
+            if isinstance(data, list):
+                error_msg = str(data)
+            else:
+                error_msg = data.get("detail")
+            response_success = response.status_text
+            msg = response.status_text
+
+            # response parse
+            pagination = {
+                "count": None,
+                "next": None,
+                "previous": None
+            }
+
+            output_data = {
+                "error": {"code": error_code, "error_details": error_msg},
+                "pagination": pagination,
+                "data": None,
+                "status": False,
+                "msg": msg if msg else str(error_msg) if error_msg else "Success" if response_success else "Failed",
+            }
+            return super().render(output_data, accepted_media_type, renderer_context)
+        return super().render(data, accepted_media_type, renderer_context)

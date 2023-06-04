6b17e8993f54b6af3ad322823411c4966c27226f
navid.rashik <navid.rashik@gmail.com>
2023-06-04 23:59:32 +0600

    added throttle

--- technotesplus/settings.py
+++ technotesplus/settings.py
@@ -139,7 +139,12 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_RENDERER_CLASSES": ("utils.response_wrapper.CustomRenderer",),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "utils.pagination.CustomLimitPagination",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 # Internationalization
